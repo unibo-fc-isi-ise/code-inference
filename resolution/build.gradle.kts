7bd7152ac96df409b39b20f4fbbe557d79dfd29f
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-03-14 20:33:45 +0100

    fix: working pure logic solver

--- resolution/build.gradle.kts
+++ resolution/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation(libs.kotlin.stdlib.jvm)
 
-    implementation(libs.tuprolog.solve)
+    implementation(libs.tuprolog.theory)
     implementation(libs.tuprolog.parser.theory)
 
     // Use the Kotlin test library.
